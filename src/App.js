429a7e675d2f1616ec4c9e5a048bee4738eeb7ae
Takashi Nishikawa <program.takashi@gmail.com>
2019-05-28 08:47:04 +0900

    [change] first change

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo, { ReactComponent } from './logo.svg';
 import './App.css';
 
-
-
-function App() {
+function TodoList(props) {
+  const todos = props.todos.map(todo => {
+    console.log(todo.id);
+    return (
+      <li key="{todo.id}">
+        <ul>
+          <li>{todo.id}</li>
+          <li>{todo.content}</li>
+          <li>{todo.color}</li>
+        </ul>
+      </li>
+    );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Hello React!</p>
-      </header>
-    </div>
+    <ul>
+      {todos}
+    </ul>
   );
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          id: 'a',
+          content: 'math',
+          color: 'tomato'
+        },
+        {
+          id: 'b',
+          content: 'lang',
+          color: 'blue'
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <TodoList
+            todos={this.state.todos}
+          />
+          <p>Hello React!</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
